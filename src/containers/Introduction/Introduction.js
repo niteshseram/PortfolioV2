caa11d18f0c608b9a33b5c40bc438cff8bdbd84a
S Nitesh Singh <niteshseram@gmail.com>
2021-05-14 20:12:52 +0530

    adding some animation using gsap

--- src/containers/Introduction/Introduction.js
+++ src/containers/Introduction/Introduction.js
@@ -1,16 +1,67 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Introduction.css";
+import { gsap, TweenLite } from "gsap";
 
 const Introduction = () => {
+  const imgRef = useRef(null);
+  const hiRef = useRef(null);
+  const nameRef = useRef(null);
+  const profRef = useRef(null);
+  const resumeRef = useRef(null);
+
+  useEffect(() => {
+    let t1 = gsap.timeline();
+    TweenLite.fromTo(
+      imgRef.current,
+      { opacity: 0, scale: 0 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+      }
+    );
+    t1.fromTo(
+      hiRef.current,
+      { y: -50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1 }
+    );
+    t1.fromTo(
+      nameRef.current,
+      { y: -50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1 }
+    );
+    t1.fromTo(
+      profRef.current,
+      { y: -50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1 }
+    );
+    t1.fromTo(
+      resumeRef.current,
+      { y: -50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1 }
+    );
+  }, []);
+
   return (
     <section id="intro">
-      <div className="image">
+      <div className="image" ref={imgRef}>
         <img src="../../assets/images/dp.png" alt="profile pic" />
       </div>
       <div className="intro-text">
-        <h1 className="hi">Hi, I'm</h1>
-        <h1 className="name">Nitesh Seram</h1>
-        <h2 className="profession">Full Stack Web Developer</h2>
+        <h1 className="hi" ref={hiRef}>
+          Hi, I'm
+        </h1>
+        <h1 className="name" ref={nameRef}>
+          Nitesh Seram
+        </h1>
+        <h2 className="profession" ref={profRef}>
+          Full Stack Web Developer
+        </h2>
+        <div className="resume" ref={resumeRef}>
+          <a className="btn resume-btn" href="resume">
+            Get Resume
+          </a>
+        </div>
       </div>
     </section>
   );
