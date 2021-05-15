45d37ef2722257ef11ceda58154c4b94e7a4c06e
S Nitesh Singh <niteshseram@gmail.com>
2021-05-15 18:58:43 +0530

    fixing some bug in animation

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./About.css";
@@ -7,62 +7,55 @@ import data from "../../data";
 gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const headingRef = useRef(null);
+  const aboutRef = useRef(null);
+  const aboutTextRef = useRef(null);
+  const aboutImageRef = useRef(null);
+
   useEffect(() => {
-    gsap.from(".about-heading-animation", {
-      duration: 3,
+    let t1 = gsap.timeline({
+      scrollTrigger: {
+        trigger: aboutRef.current,
+        markers: false,
+        start: "top 50%",
+        end: "bottom top",
+        toggleActions: "restart complete reverse reset",
+      },
+    });
+    t1.from(headingRef.current, {
+      duration: 2,
       x: "-100",
       opacity: 0,
       ease: "ease-in",
-      scrollTrigger: {
-        trigger: ".about-heading-animation",
-        markers: true,
-        start: "top 90%",
-        end: "bottom 0%",
-        toggleActions: "restart complete reverse reset",
-      },
     });
-    gsap.from(".text", {
-      duration: 3,
+    t1.from(aboutTextRef.current, {
+      duration: 2,
       x: "-100",
       opacity: 0,
       ease: "ease-in",
-      scrollTrigger: {
-        trigger: ".text",
-        markers: true,
-        start: "top 50%",
-        end: "bottom 0%",
-        toggleActions: "restart complete reverse reset",
-      },
     });
-    gsap.from(".about-img", {
-      duration: 3,
-      x: -100,
+    t1.from(aboutImageRef.current, {
+      duration: 2,
+      x: 100,
       opacity: 0,
       ease: "ease-in",
-      scrollTrigger: {
-        trigger: ".about-img",
-        markers: true,
-        start: "top 50%",
-        end: "bottom 0%",
-        toggleActions: "restart complete reverse reset",
-      },
     });
-  }, []);
+  });
 
   return (
-    <section id="about">
-      <div className="section-heading about-heading-animation">
+    <section id="about" ref={aboutRef}>
+      <div className="section-heading" ref={headingRef}>
         <h1 data-text="A little about me">ABOUT</h1>
       </div>
       <section className="section-content">
-        <div className="about-text">
-          <p className="text">{data.aboutMe[0]}</p>
+        <div className="about-text" ref={aboutTextRef}>
+          <p>{data.aboutMe[0]}</p>
           <br />
-          <p className="text">{data.aboutMe[1]}</p>
+          <p>{data.aboutMe[1]}</p>
           <br />
-          <p className="text">{data.aboutMe[2]}</p>
+          <p>{data.aboutMe[2]}</p>
         </div>
-        <div className="about-img">
+        <div className="about-img" ref={aboutImageRef}>
           <img src={data.profileUrl} alt="profile pic" />
         </div>
       </section>
